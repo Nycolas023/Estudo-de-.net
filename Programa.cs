d08e7f8f300cd73065be983fffaaf6bc3497328b
Nycolas F <79328874+Nycolas023@users.noreply.github.com>
2022-08-17 10:57:40 -0300

    Programa de Média

---
+++ Programa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+        int a, b, c;
+
+            Console.WriteLine("Digite seu primeiro numero: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu segundo numero: ");
+            b = int.Parse(Console.ReadLine());
+
+            c = (a + b)/2;
+
+            Console.WriteLine("Seu primeiro número: "+a+"\n Seu segundo número: "+b+"\n sua média é: "+c);
+
+            if (c < 6)
+            {
+                Console.WriteLine("Sua média é abaixo de 6!!");
+                c -= 10;
+                
+                Console.WriteLine("Para você chegar a uma média aceitavel é necessario: "+ Math.Abs(c));
+            }
+            Console.ReadKey();
+        }
+    }
+}
