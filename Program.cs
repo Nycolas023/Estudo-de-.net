59ccd2898aa171e08c5bd6438ce17fb71cfe2b5a
Nycolas F <79328874+Nycolas023@users.noreply.github.com>
2022-08-24 12:30:35 -0300

    Jogo advinha
    Um jogo criando em C#.net treinando o random, laço e estrutura de condições.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random resp = new Random();
+            int num = resp.Next(1, 10);
+            int respU;
+
+            Console.WriteLine("Chute um numero de 1 a 10 humano burru: ");
+            try
+            {
+                respU = int.Parse(Console.ReadLine());
+
+                while (respU >= 0)
+                {
+                    if (respU == num)
+                    {
+                        Console.WriteLine("O numero da vez era: " + num);
+                        Console.WriteLine("Você digitou: " + respU);
+                        Console.WriteLine("----Parabéns!----");
+                        break;
+                    }
+
+                    if (respU != num)
+                    {
+                        Console.WriteLine("\nA proximidade do seu numero é de: " + (Math.Abs(respU - num)));
+                        Console.WriteLine("Você digitou: " + respU);
+                        Console.WriteLine("Você errou!");
+
+                        Console.WriteLine("\nTente novamente: \n");
+                        respU = int.Parse(Console.ReadLine());
+                        continue;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro na digitação, tente novamente");
+            }
+            Console.ReadKey();
+        }
+    }
+}
