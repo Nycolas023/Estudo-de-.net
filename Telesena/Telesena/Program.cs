4a427eaaa3dac33e210b980c0c3ee4ca54b078ac
Nycolas F <79328874+Nycolas023@users.noreply.github.com>
2022-08-26 14:21:13 -0300

    Add files via upload

---
+++ Telesena/Telesena/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Telesena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string b, bl = "sim";
+            Random rdn = new Random();
+
+            while (bl == "sim" || bl == "s")
+            {
+                try
+                {
+                    Console.WriteLine("Coloque quantos números da telesena quer sortear: ");
+                    a = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < a; i++)
+                    {
+                        Console.WriteLine("Seu " + (i + 1) + "º número é: " + rdn.Next(1, 60));
+                    }
+
+                    Console.WriteLine("\nDeseja continuar? ");
+                    b = Console.ReadLine();
+                    bl = b.ToLower();
+
+
+                    if (bl != "sim" && bl != "s")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\nEscreva um número inteiro ou não digite teclas");
+                }
+            }
+        }
+    }
+}
